dd8cb20359242a276c4ad9b5336e8e270e48e310
Trinishamuthavarapu <153422017+Trinishamuthavarapu@users.noreply.github.com>
2024-03-16 18:47:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './components/Counter.jsx';
+
+function App() {
+  return (
+    <div className='counter-container'>
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
+
